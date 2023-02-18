82b91cba9501c5b10bb4c19771db5b54754bb115
Will Duquette <will@wjduquette.com>
2023-02-18 15:57:05 -0800

    Nice tool.

--- app/src/main/java/destiny2/ArmorComparator.java
+++ app/src/main/java/destiny2/ArmorComparator.java
@@ -1,34 +1,41 @@
 package destiny2;
 
+import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
+import java.util.Map;
 
 /**
  * Sorts by the primary stats, in order, followed by the sum of the remainder.
  */
 public class ArmorComparator implements Comparator<ArmorSet> {
-    private final List<Stat> primary;
+    private final Map<Stat,Double> weights;
 
-    public ArmorComparator(Stat... primary) {
-        this.primary = List.of(primary);
-    }
-
-    public ArmorComparator(List<Stat> primary) {
-        this.primary = primary;
+    public ArmorComparator(Map<Stat,Double> weights) {
+        this.weights = weights;
     }
 
     @Override
     public int compare(ArmorSet set1, ArmorSet set2) {
-        var v1 = set1.stats();
-        var v2 = set2.stats();
-        var sum1 = primary.stream().mapToInt(v1::get).sum();
-        var sum2 = primary.stream().mapToInt(v2::get).sum();
+        var stats1 = set1.stats();
+        var stats2 = set2.stats();
+        var sum1 = 0.0;
+        var sum2 = 0.0;
 
-        return Integer.compare(sum1, sum2);
+        for (var s : Stat.values()) {
+            sum1 += stats1.get(s)*weights.get(s);
+            sum2 += stats2.get(s)*weights.get(s);
+        }
+
+        return Double.compare(sum1, sum2);
     }
 
     @Override
     public String toString() {
-        return String.join("+", primary.stream().map(Stat::toString).toList());
+        var list = new ArrayList<String>();
+
+        for (var s : Stat.values()) {
+            list.add(s + "=" + String.format("%03.1f", weights.get(s)));
+        }
+        return String.join(", ", list);
     }
 }
